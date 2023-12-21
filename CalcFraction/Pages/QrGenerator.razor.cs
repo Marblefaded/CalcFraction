f98b6a0b7e968637969099b42003b074550cc20f
Marblefaded <45118101+Marblefaded@users.noreply.github.com>
2023-12-21 20:03:10 +0700

    VS: Qr

---
+++ CalcFraction/Pages/QrGenerator.razor.cs
@@ -0,0 +1,29 @@
+﻿/*using Microsoft.AspNetCore.Components;
+using QRCoder;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace CalcFraction.Pages
+{
+    public class QrGeneratorView: ComponentBase
+    {
+        public string QRCodeText { get; set; }
+        public string QRByte = "";
+
+        public void GenerateQRCode()
+        {
+            if (!string.IsNullOrEmpty(QRCodeText))
+            {
+                using MemoryStream ms = new();
+                QRCodeGenerator qrCodeGenerate = new();
+                QRCodeData qrCodeData = qrCodeGenerate.CreateQrCode(QRCodeText, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new(qrCodeData);
+                using Bitmap qrBitMap = qrCode.GetGraphic(20);
+                qrBitMap.Save(ms, ImageFormat.Png);
+                string base64 = Convert.ToBase64String(ms.ToArray());
+                QRByte = string.Format("data:image/png;base64,{0}", base64);
+            }
+        }
+    }
+}
+*/
